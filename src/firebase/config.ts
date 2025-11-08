429ae28e1f9f67d224725337c34783de6d10b547
Rafael Ferreira Souza <rafaelf.rfs@gmail.com>
2025-11-08 00:15:29 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "magnetai-4h4a8",
+  "appId": "1:210739580533:web:07c2e35aaa909693ed723c",
+  "apiKey": "AIzaSyALRps1FyfrS8P3SxTEhpU-0m3Mb58k_1w",
+  "authDomain": "magnetai-4h4a8.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "210739580533"
+};
