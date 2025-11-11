ea0b73daad0adbd0c9504b5fe736334ce1103522
rfsouza1492 <82185044+rfsouza1492@users.noreply.github.com>
2025-11-11 13:11:58 -0300

    Remove deprecated Firebase configuration and related files

--- Users/rafaelsouza/Development/GCP/studio/src/firebase/config.ts
+++
@@ -1,9 +0,0 @@
-
-export const firebaseConfig = {
-  "projectId": "magnetai-4h4a8",
-  "appId": "1:210739580533:web:07c2e35aaa909693ed723c",
-  "apiKey": process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  "authDomain": "magnetai-4h4a8.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "210739580533"
-};
